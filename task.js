1839f52c4dd9d93f9bdb2f9f9a04577acaedd298
Yuliya <vsggdfaf@gmail.com>
2022-03-01 14:46:57 +0300

    Ответ в к

---
+++ task.js
@@ -0,0 +1,8 @@
+var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
+
+function returnLast(arr) {
+  return arr.at(-1);
+}
+
+const item = returnLast(words);
+console.log(item);
